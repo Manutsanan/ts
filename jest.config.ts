cf1903d992e3df9577201bea9c1f3ba9d7459783
Manutsanan <manutsanan.golf@gmail.com>
2024-04-27 15:13:39 +0700

    Update

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types';
+// Sync object
+const config: Config.InitialOptions = {
+  moduleDirectories: ['node_modules', 'src'],
+  verbose: true,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+};
+export default config;
